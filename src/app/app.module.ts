6a0a316768cd829fb2f95adfb68469e231fada1f
rashidmajeed <rashid.caliber@gmail.com>
2020-07-04 03:02:07 +0200

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { CustomersComponent } from './components/customers/customers.component';
+import { LoansComponent } from './components/loans/loans.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CustomersComponent,
+    LoansComponent
   ],
   imports: [
     BrowserModule
